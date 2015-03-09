f9f5c050210ff042b80141a33d9d0668741626e1
latotty <latotty91@gmail.com>
2015-03-09 11:55:42 +0100

    jshint fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 (function() {
 
 $(window).resize(function(){
-  var ratio = $('.content').width()/1182;
-  $('.wrapper').css('height', 764 * ratio);
+  var ratio = $(".content").width()/1182;
+  $(".wrapper").css("height", 764 * ratio);
 
-  $('.container').css('transform', 'scale('+ratio+')');
+  $(".container").css("transform", "scale("+ratio+")");
 });
 $(document).ready(function(){
-  var ratio = $('.content').width()/1182;
-  $('.wrapper').css('height', 764 * ratio);
+  var ratio = $(".content").width()/1182;
+  $(".wrapper").css("height", 764 * ratio);
 
-  $('.container').css('transform', 'scale('+ratio+')');
+  $(".container").css("transform", "scale("+ratio+")");
 
   var bg = "CodingSans_hex.png";
   var bgs = ["nodejs_hex.png",
@@ -54,7 +54,7 @@ $(document).ready(function(){
       var rnd1 = Math.random()*brand.length;
       var img1 = brand.splice(rnd1, 1);
       var url1 = urls.splice(rnd1, 1);
-      if (!brand.length) break;
+      if (!brand.length) { break; }
       var rnd2 = Math.random()*brand.length;
       var img2 = brand.splice(rnd2, 1);
       var url2 = urls.splice(rnd2, 1);
@@ -72,16 +72,16 @@ $(document).ready(function(){
 
     while ( i < brand.length ) {
       var img = $(brand[i]);
-      img.css("transform" , 'translate('+((img.attr('col')-1)*220)+'px, '+((img.attr('row')-1)*126)+'px)');
-      img.attr("href", urls[i]); 
+      img.css("transform" , "translate("+((img.attr("col")-1)*220)+"px, "+((img.attr("row")-1)*126)+"px)");
+      img.attr("href", urls[i]);
       i++;
     }
 
   }
 
   function swp(a, b) {
-    swap($(a), $(b), 'col');
-    swap($(a), $(b), 'row');
+    swap($(a), $(b), "col");
+    swap($(a), $(b), "row");
   }
 
   function swap(a, b, type){
@@ -90,7 +90,7 @@ $(document).ready(function(){
     b.attr(type, tmp);
   }
 
-  function loadImages(){
+  function loadImages() {
     shuffle();
     var i = 0, j = 0;
 
@@ -98,14 +98,14 @@ $(document).ready(function(){
       var img = $(imgs[i]);
 
       if (i === 6) {
-        img.css("background-image", 'url(assets/'+bg+')');
+        img.css("background-image", "url(assets/"+bg+")");
       } else {
-        img.css("background-image", 'url(assets/'+bgs[j%(bgs.length)]+')');
+        img.css("background-image", "url(assets/"+bgs[j%(bgs.length)]+")");
         j++;
       }
-      img.css("top" , '0');
-      img.css("left" , '0');
-      img.css("transform" , 'translate('+((img.attr('col')-1)*220)+'px, '+((img.attr('row')-1)*126)+'px)');
+      img.css("top" , "0");
+      img.css("left" , "0");
+      img.css("transform" , "translate("+((img.attr("col")-1)*220)+"px, "+((img.attr("row")-1)*126)+"px)");
       i++;
     }
 
